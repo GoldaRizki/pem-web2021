73227197d2a502efefb4620132fae098a2128c23
GoldaRizki <90323978+GoldaRizki@users.noreply.github.com>
2021-09-13 14:51:17 +0700

    Bikin folder baru (Minggu 1)

---
+++ Minggu 1/tampilversi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> 
+
+<!--
+
+Hanya untuk menampilkan versi php saja
+
+-->
+
+<html>
+<head>
+<title>YNTKTS</title>
+</head>
+
+<body>
+<!-- menampilkan versi php -->
+<?php
+phpinfo();
+?>
+
+</body>
+
+</html>
